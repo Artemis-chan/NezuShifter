c6c7f289587bf89dbef61c7bbab8517111c0790f
Artemis <skamickrahaman12@gmail.com>
2022-05-29 12:22:33 +0530

    remove redundant headers

--- controller_emu/controller_emu_linux.cpp
+++ controller_emu/controller_emu_linux.cpp
@@ -1,16 +1,5 @@
 #include <stdio.h>
-#include <windows.h>
-#include <ViGEm/Client.h>
-#include <xinput.h>
 #include <iostream>
-
-#pragma comment(lib, "setupapi.lib")
-
-#include "controller_emu.h"
-
-PVIGEM_CLIENT client;
-PVIGEM_TARGET pad;
-XINPUT_STATE state;
 
 int controller_emu_init() {
     return 0;
